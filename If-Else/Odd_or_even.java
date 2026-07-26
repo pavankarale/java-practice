999b75112338fc3a86601d62c9abc3e6fdaa3e83
pavankarale <pavankarale251@gmail.com>
2026-07-26 16:49:04 +0530

    Add files via upload
    Add java If-else programs

---
+++ If-Else/Odd_or_even.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Odd_or_even {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Odd even calculator\n");
+        System.out.print("Please enter your number: ");
+        int num = input.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("Your number is an even number");
+        } else {
+            System.out.println("Your number is a odd number.");
+        }
+    }
+}
---
+++ If-Else/Postitive_or_Negative.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Postitive_or_Negative {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Number checker\n");
+        System.out.print("Please enter your number: ");
+        int num = input.nextInt();
+
+        if (num > 0) {
+            System.out.println("Your number is positive");
+        } else if (num == 0) {
+            System.out.println("Your number is zero");
+        } else {
+            System.out.println("Your number is negative");
+        }
+    }
+}
